9d7a635be0fd77ee793c139daf1eb0079a0a5089
Jason <jas0nle@yahoo.com>
2019-09-17 15:33:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fetchBooks() {
-
+  let fetchGOT = fetch('https://anapioficeandfire.com/api/books')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      renderBooks(json);
+    });
+  return fetchGOT
 }
 
 function renderBooks(json) {
